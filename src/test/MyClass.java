9af43bea550832c28b322d5db4134017bfc9bd46
rajadilip <dilip.raja@deepvalue.net>
2016-07-12 08:00:30 -0400

    Extending Default test case class

--- src/test/MyClass.java
+++ src/test/MyClass.java
@@ -1,5 +1,6 @@
 package test;
 
+import junit.framework.TestCase;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,13 +10,19 @@ import static org.junit.Assert.assertTrue;
 /**
  * Created by dilip on 7/12/16.
  */
-public class MyClass {
-    int number =25;
-    @Test
-    public void testAdd() {
-       // assertTrue(number < 10);
-        assertTrue("Number is not less than 10" , number < 10);
+public class MyClass extends TestCase {
+    private int number = 35;
 
+    protected void setUp() throws Exception {
+        super.setUp();
+        JUnitTestManager.getInstance().setJUnitTestRunning(true);
     }
+    protected void tearDown() throws Exception
+    {
+        super.tearDown();
     }
-
+    public void testNumber()
+    {
+        assertEquals(35 , number);
+    }
+}
