84e772d60743aa841d204d690e9e23d939da31c3
rajadilip <dilip.raja@deepvalue.net>
2016-07-12 05:23:56 -0400

    Inital test

---
+++ src/test/MyClass.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * Created by dilip on 7/12/16.
+ */
+public class MyClass {
+
+        public static void main(String args[])
+        {
+            System.out.println("Welcome to Jenkins");
+        }
+    }
+
