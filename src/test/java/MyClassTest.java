e234b267f398eb094a08f2acacff942e823e2987
rajadilip <dilip.raja@deepvalue.net>
2016-07-13 07:44:40 -0400

    Added JUnit classes

---
+++ src/test/java/MyClassTest.java
@@ -0,0 +1,20 @@
+package test.java;
+
+import junit.framework.TestCase;
+import main.MyClass;
+import org.junit.Test;
+
+/**
+ * Created by dilip on 7/13/16.
+ */
+public class MyClassTest extends TestCase{
+
+    public void testConcatenate() {
+        MyClass myUnit = new MyClass();
+
+        String result = myUnit.concatenate("one", "two");
+
+        assertEquals("onetwo", result);
+
+    }
+}
